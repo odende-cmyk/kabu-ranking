fe42c9e24b251e0e01d6560fd023a605c2d96e3d
岡田凛太郎 <okadarintarou@okadarintarounoMacBook-Pro.local>
2026-04-27 10:29:57 +0900

    add adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +29,17 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col">
+  <Script
+    async
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4287797882027569"
+    crossOrigin="anonymous"
+    strategy="afterInteractive"
+  />
+  {children}
+</body>
+        {children}</body>
     </html>
   );
 }
